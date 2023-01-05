6fd7dcac8d051b5a4df262335bb326327ca43ad7
KevZ1209 <kevinbowditch11@gmail.com>
2023-01-04 21:06:42 -0800

    add intro animations, add new videos and descriptions for personal life section

---
+++ js/index.js
@@ -0,0 +1,20 @@
+setTimeout(function() {
+    $("#intro-1").animate({
+        top: '0',
+        opacity: '0.7'
+    }, 1000)
+}, 1100)
+
+setTimeout(function() {
+    $("#intro-2").animate({
+        top: '0',
+        opacity: '0.7'
+    }, 1000)
+}, 2200)
+
+setTimeout(function() {
+    $("#intro-3").animate({
+        top: '0',
+        opacity: '0.7'
+    }, 1000)
+}, 3300)
